45f4f47952f9852de9e51b8c9bf926274d8bc1d6
Zelberman <azelenskiy87@gmail.com>
2022-11-29 14:32:04 +0500

    Name-Masha

---
+++ Example005name/Program.cs
@@ -0,0 +1,14 @@
+﻿// имена
+
+Console.WriteLine("Привет, назови свое имя");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "маша")
+    {
+        Console.WriteLine("Hello my friend, Masha");
+    }
+else
+    {
+        Console.WriteLine("Hello, ");
+        Console.WriteLine(username);
+    }
