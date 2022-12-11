167dad9b51946f4e5eaebb1e73f39e3382dec7ce
Zelberman <azelenskiy87@gmail.com>
2022-12-12 01:56:42 +0500

    end

--- 4DZ-seminar/Program.cs
+++ 4DZ-seminar/Program.cs
@@ -36,26 +36,45 @@ int Sumnumber(int A3, int B3)
     return sumnum;
 }
 
-int powAB = Sumnumber(A2,B2);
-Console.WriteLine(powAB);
+int AB = Sumnumber(A2, B2);
+Console.WriteLine(AB);
 
 
 
 // Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
-// Console.WriteLine("Введите число");
-// int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите число");
+int N = int.Parse(Console.ReadLine()!);
 // int[] array = new int[N]
 
-// int Function(int[] array1, N1)
-// {
+int GetSum(int number)
+{
+    int plus = 0;
+    int sumplus = 0;
+    while (number >= 1)
+    {
+        plus = number % 10;
+        number = number / 10;
+        sumplus = sumplus + plus;
+    }
+    return sumplus;
+}
 
-//     int sum = 0;
-//     int index = 0;
-//     while (index < N1)
-//     {
-//         sum = sum + array[index]
-//         index++;
-//     }
-// }
+Console.WriteLine($"сумма чисел числа {N} равна {GetSum(N)}");
 
+// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Задача №29. Задаем массив из 8 Элементов");
+int[] U = new int[8];
+void Getarray(int[] U1)
+{
+    int length = U1.Length;
+    int position = 0;
+    while (position < length)
+    {
+        U1[position] = new Random().Next(0, 10);
+       Console.Write($"{U1[position]}\t");
+        position++;
+    }
+}
+Getarray(U);
 
