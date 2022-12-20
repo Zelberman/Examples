38c4eb84550f07033d9e46c28ac3d8c515f513a5
Andrew <118960416+Zelberman@users.noreply.github.com>
2022-12-21 03:25:54 +0500

    Delete Example003sum directory

--- Example003sum/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-int NumberA = new Random().Next(1,10); // случайное число 1-9 (max-1)
-Console.WriteLine(NumberA);
-int NumberB = new Random().Next(0,10); // случайное число от 0 до 10
-Console.WriteLine(NumberB);
-int result = (NumberA + NumberB) ;
-Console.WriteLine(result);
-
-
